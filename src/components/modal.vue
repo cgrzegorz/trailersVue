fca746ac111cf7adf8a18965d0bfb9dcc5c6af13
greg cieslak <g.cieslak@asper.net.pl>
2020-04-03 09:57:25 +0200

    add fix mobile

--- src/components/modal.vue
+++ src/components/modal.vue
@@ -4,15 +4,16 @@
       <div class="modal-wrapper">
         <div class="modal-container">
           <button class="close" @click="$emit('close')">X</button>
-          <iframe
-            v-if="trailerAdress.length > 0"
-            width="650"
-            height="405"
-            :src="`https://www.youtube.com/embed/${trailerAdress[0].key}`"
-            frameborder="0"
-            allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
-            allowfullscreen
-          ></iframe>
+          <div class="video-container"  v-if="trailerAdress.length > 0">
+            <iframe
+              width="650"
+              height="405"
+              :src="`https://www.youtube.com/embed/${trailerAdress[0].key}`"
+              frameborder="0"
+              allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"
+              allowfullscreen
+            ></iframe>
+          </div>
           <p v-else>Brak Filmu w Bazie Danych</p>
         </div>
       </div>
